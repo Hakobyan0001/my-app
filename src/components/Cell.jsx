988a37a4a97b2bf51acf66595cab3315e5d16d6f
Hakobyan0001 <123662211+Hakobyan0001@users.noreply.github.com>
2023-07-20 19:38:02 +0400

    only one ball can be selected

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,14 +1,26 @@
 import Ball from "./Ball";
 import { useState } from "react";
 
+
+let selectedBall = false
+
 function Cell({ hasBall, id }) {
   const [isActive, setIsActive] = useState(false);
-
   function handelCellClick() {
-    if (isActive === false) {
-      setIsActive(true);
-    } else if (isActive === true) {
-      setIsActive(false);
+    if (hasBall) {
+      if (selectedBall === false && isActive === false) {
+        selectedBall = true;
+        setIsActive((prevValue) => { return !prevValue; })
+      }
+      else if (selectedBall === true && isActive === false) {
+
+        return null;
+
+      }
+      else if (selectedBall === true && isActive === true) {
+        selectedBall = false;
+        setIsActive((prevValue) => { return !prevValue; })
+      }
     }
   }
   return (
