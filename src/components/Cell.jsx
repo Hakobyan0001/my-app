59fbd6f2fc1704b28cb8a01cd46163ed2fc465f4
Hakobyan0001 <123662211+Hakobyan0001@users.noreply.github.com>
2023-07-20 20:13:31 +0400

    new moveball function

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,33 +1,41 @@
+import { moveBall } from "../util";
 import Ball from "./Ball";
 import { useState } from "react";
 
-
-let selectedBall = false
-
+let selectedBall = {
+  areBall: false,
+  id: null
+}
 function Cell({ hasBall, id }) {
   const [isActive, setIsActive] = useState(false);
+
   function handelCellClick() {
+
     if (hasBall) {
-      if (selectedBall === false && isActive === false) {
-        selectedBall = true;
+      if (selectedBall.areBall === false && isActive === false) {
+        selectedBall.areBall = true;
+        selectedBall.id = { id }
         setIsActive((prevValue) => { return !prevValue; })
       }
-      else if (selectedBall === true && isActive === false) {
-
+      else if (selectedBall.areBall === true && isActive === false) {
         return null;
-
       }
-      else if (selectedBall === true && isActive === true) {
-        selectedBall = false;
+      else if (selectedBall.areBall === true && isActive === true) {
+        selectedBall.areBall = false;
+        selectedBall.id = null;
         setIsActive((prevValue) => { return !prevValue; })
       }
+    } else if (!hasBall && selectedBall.areBall) {
+      moveBall(selectedBall.id, id)
     }
   }
+
   return (
     <div className="cell" onClick={handelCellClick} id={id}>
       {hasBall ? <Ball isActive={isActive} /> : null} {id}
     </div>
   );
+
 }
 
 export default Cell;
--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,27 @@
+function getEmptyCells(board) {
+  let emptyCellsIndices = board.map((element, index) => index);
+
+  return emptyCellsIndices;
+}
+
+function getRandomIndices(emptyCellsIndices) {
+
+  let indices = [...emptyCellsIndices];
+  const BALLS_COUNT = 3;
+  let ballsIndices = [];
+
+  for (let i = 0; i < BALLS_COUNT; i++) {
+
+    const RANDOM_INDEX = Math.floor(Math.random() * indices.length);
+    const chosenIndex = indices[RANDOM_INDEX];
+
+    indices.splice(RANDOM_INDEX, 1);
+    ballsIndices.push(chosenIndex);
+  }
+
+  return ballsIndices;
+}
+
 function addBallinBoard(board, indices) {
   indices.forEach((index) => {
     board[index] = {
@@ -7,25 +31,8 @@ function addBallinBoard(board, indices) {
   });
 }
 
-function getEmptyCells(board) {
-  let emptyCellsIndices = board.map((element, index) => index);
+function moveBall(from, to) {
 
-  return emptyCellsIndices;
 }
 
-function getRandomIndices(emptyCellsIndices) {
-  let indices = [...emptyCellsIndices];
-  const BALLS_COUNT = 3;
-  let ballsIndices = [];
-
-  for (let i = 0; i < BALLS_COUNT; i++) {
-    const RANDOM_INDEX = Math.floor(Math.random() * indices.length);
-    const chosenIndex = indices[RANDOM_INDEX];
-
-    indices.splice(RANDOM_INDEX, 1);
-    ballsIndices.push(chosenIndex);
-  }
-  return ballsIndices;
-}
-
-export { getEmptyCells, getRandomIndices, addBallinBoard };
+export { getEmptyCells, getRandomIndices, addBallinBoard, moveBall };
